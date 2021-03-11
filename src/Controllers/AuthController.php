32c6f47fc81ecc67e8bd50a6c45582b3c4117d67
Nam tran <provn8x@gmail.com>
2021-03-12 02:03:32 +0700

    fix login redirect

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -65,7 +65,7 @@ class AuthController extends Controller
         if (! empty($code)) {
             $token = KeycloakWeb::getAccessToken($code);
             if (Auth::validate($token)) {
-                $url = route('home');
+                $url = env('ROUTE_PREFIX');
                 return redirect($url);
             }
         }
